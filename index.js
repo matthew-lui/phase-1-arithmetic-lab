25a711b413f59798bb9df2847611ad506bbdeb63
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-09-21 14:57:56 -0400

    commit tree da69089db0e92c6147124e2d9b0f3c0b212f82cf

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
+console.log(multiply);
 
-// Write your code here
+let random = 1;
+Math.random();
+
+let num3 = 6;
+let num4 = 2;
+let mod = num3 - num4
+console.log(mod);
+
+let max = Math.max(1, 3, 20);
